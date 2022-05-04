b4c8242b7c6d2b984f8295918e739f170f35ad93
Mudit Mahajan <muditmahajan21@gmail.com>
2022-05-04 10:46:56 +0000

    Update Day06/Palindrome Linked List.cpp, Day06/README.md

---
+++ Day06/Palindrome Linked List.cpp
@@ -0,0 +1,35 @@
+bool isPalindrome(ListNode* head) {
+        if(!head or !head -> next) return true;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast -> next and fast -> next -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+                
+        ListNode* temp = slow -> next;
+        ListNode* prev = nullptr;
+        ListNode* nxt = nullptr;
+        
+        while(temp) {
+            nxt = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = nxt;
+        }
+                
+        slow -> next = prev;
+        slow = slow -> next;
+        ListNode* dummy = head;
+        
+        while(slow) {
+            cout << slow -> val << " ";
+            if(dummy -> val != slow -> val) return false;
+            dummy = dummy -> next;
+            slow = slow -> next;
+        }
+        
+        return true;
+    }
--- Day06/README.md
+++ Day06/README.md
@@ -11,3 +11,17 @@ Link: [https://leetcode.com/problems/linked-list-cycle/]
 - Outside the loop, return false.
 
 Time Complexity: O(n).
+
+## Palindrome Linked List
+
+Link: [https://leetcode.com/problems/palindrome-linked-list/]
+
+- If head is null or next of head is null, return true.
+- Find the middle of the linked list by using the slow and fast pointer method.
+- Create a temp node pointing to next of slow.
+- Reverse the list from temp and assign prev to slow -> next.
+- Take a dummy node pointing to the head of the linked list and start traversing from the middle as well as the head.
+- If any element is not same at the two points, then return false.
+- Else, at the end of the ;loop, return true.
+
+Time Complexity: O(n)
